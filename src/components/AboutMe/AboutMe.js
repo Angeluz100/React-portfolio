74d89655b4f0331609fdf1f63724806edcefec5f
Angeluz100 <francotgutierrez@gmail.com>
2021-05-13 16:35:01 -0400

    Changing title

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
                 <div className="col-lg-6 col-xm-12">
                     <h1 className="about-heading">about me</h1>
                 <p>
-                    Hello! I am Franco. I am a full stack software engineer motivated to learn and grow in a challenging, ever-changing field. I strive to engineer simple, effective, & powerful solutions for users on and around the Web. I have recently been working on learning different languages, and how to solve problems in those languages. I was drawn to software engineering because I wanted to deepen my technical expertise, and make a career change into a highly demanded and highly challenging field, with greater opportunities for growth.
+                    Hello! I am Franco. I am a full stack software engineer motivated to learn and grow in a challenging, ever-changing field. I strive to enginer simple, effective, & powerful solutions for users on and around the Web. I have recently been working on learning different languages, and how to solve problems in those languages. I was drawn to software engineering because I wanted to deepen my technical expertise, and make a career change into a highly demanded and highly challenging field, with greater opportunities for growth.
 
                 </p>
                 </div>
